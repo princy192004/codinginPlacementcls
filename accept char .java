e2b92bd785a8e6a73b4c8e2181339fb5bc457c53
princy192004 <159789341+princy192004@users.noreply.github.com>
2025-07-31 06:13:22 +0530

    Add files via upload

---
+++ accept char .java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char n;
+        n = sc.next().charAt(0);
+        System.out.println(n);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
