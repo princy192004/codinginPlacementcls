591a1a26efc69f8558adc25e426b4ba89897c3b1
princy192004 <159789341+princy192004@users.noreply.github.com>
2025-07-31 22:03:58 +0530

    Add files via upload

---
+++ Day2CodePrograms/2. odd or even using conditional construct.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        if( a % 2 == 1 )
+            System.out.println("ODD");
+        else 
+            System.out.println("EVEN");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
