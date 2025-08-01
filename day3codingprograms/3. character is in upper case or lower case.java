ad0f8995d7f808b767c87d24b41937da55e1adfe
princy192004 <159789341+princy192004@users.noreply.github.com>
2025-08-01 20:35:36 +0530

    Create 3. character is in upper case or lower case.java

---
+++ day3codingprograms/3. character is in upper case or lower case.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ab;
+        ab = sc.next().charAt(0);
+        if (ab >= 65 && ab <= 90)
+            System.out.println("UPPERCASE");
+        else
+            System.out.println("LOWERCASE");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
