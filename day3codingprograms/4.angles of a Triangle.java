4a81daf8a71464bb99a643b3700b01905b4bc74c
princy192004 <159789341+princy192004@users.noreply.github.com>
2025-08-01 20:36:46 +0530

    Create  4.angles of a Triangle.java

---
+++ day3codingprograms/4.angles of a Triangle.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        if((a + b + c) == 180)
+            System.out.println("Valid");
+        else 
+            System.out.println("Not Valid");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
