458192b721c3807a995c7bfe687ecc6caf5d326e
princy192004 <159789341+princy192004@users.noreply.github.com>
2025-08-05 22:04:18 +0530

    Create 4. Odd numbers from 1 to n.java

---
+++ Day5coding/4. Odd numbers from 1 to n.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a,i;
+        a = sc.nextInt();
+        for(i=1; i <= a; i++){
+            if(!(i%2 == 0)){
+                System.out.print(i + " ");
+                }
+            }
+        }
+    }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
