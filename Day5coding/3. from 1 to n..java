093ffd2cd4df43fd452f65f17782e426b503a4a3
princy192004 <159789341+princy192004@users.noreply.github.com>
2025-08-05 22:03:09 +0530

    Create 3. from 1 to n..java

---
+++ Day5coding/3. from 1 to n..java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a ,i ;
+        a = sc.nextInt();
+        for(i=1; i <=a;i++){
+            System.out.print(i + " ");
+            }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
